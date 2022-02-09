a13e4c0933ad47a49c37074c5aa2084a3dad913d
Mikael Brockman <mikael@brockman.se>
2022-02-09 13:34:57 +0200

    Fix AGPL file headers

--- wisp-tidy.c
+++ wisp-tidy.c
@@ -6,7 +6,7 @@
  * the Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful, but WITHOUT
+ * Wisp is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
