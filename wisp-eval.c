f9b3d972eb16b1ce2a62c789065b0f76db725f5c
Mikael Brockman <mikael@brockman.se>
2022-02-10 14:53:16 +0200

    Fix AGPL headers

--- wisp-eval.c
+++ wisp-eval.c
@@ -6,10 +6,10 @@
  * the Free Software Foundation, either version 3 of the License, or (at
  * your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
- * License for more details.
+ * Wisp is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
+ * Public License for more details.
 
  * You should have received a copy of the GNU Affero General Public
  * License along with Wisp. If not, see <https://www.gnu.org/licenses/>.
