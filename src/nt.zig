4a8bc031aa825dec5e166e4666b17ca4dc6ee1f1
Mikael Brockman <mikael@brockman.se>
2022-02-19 01:05:24 +0200

    Zig NT: some funky stuff

--- src/nt.zig
+++ src/nt.zig
@@ -2,48 +2,76 @@ const std = @import("std");
 const expectEqual = std.testing.expectEqual;
 const assert = std.debug.assert;
 
-test "foo" {
-    try expectEqual(1, 1);
-}
-
-pub const SymbolData = struct {
-    name: []const u8,
-    package: u14,
-};
-
-pub const Package = struct {
-    symbolMap: std.StringHashMap(u15),
-};
-
-pub const Cons = [2]u32;
-
 pub const Tag1 = enum {
     fixnum,
     nil,
-    cons,
     symbol,
+    cons,
+    closure,
     primop,
+    plan,
     glyph,
 };
 
 pub fn type1(x: u32) Tag1 {
-    if (x & 0b11 == 0) {
-        return .fixnum;
-    } else if (x == 1) {
+    if (x == 1) {
         return .nil;
-    } else if (x & 3 == 0b11) {
-        return .cons;
-    } else if (x & ~@as(u21, 0) == ~@as(u21, 0)) {
-        return .glyph;
-    } else {
-        unreachable;
     }
+
+    return switch (@intCast(u3, x & 0b111)) {
+        0b000 => .fixnum,
+        0b001 => .symbol,
+        0b010 => .cons,
+        0b011 => .closure,
+        0b100 => .fixnum,
+        0b101 => .primop,
+        0b110 => .plan,
+        0b111 => switch (x & ~@as(u11, 0)) {
+            0b111 => .glyph,
+            else => unreachable,
+        },
+    };
 }
 
-pub const Wisp = struct {
-    symbolData: std.MultiArrayList(SymbolData),
-    packages: std.ArrayList(Package),
+pub const Symbol = struct {
+    name: u29,
+    package: u14,
 };
+
+pub const Package = struct {
+    name: u29,
+    symbolMap: std.StringHashMap(u15),
+};
+
+pub const Cons = struct {
+    car: u32,
+    cdr: u32,
+};
+
+pub const Closure = struct {
+    body: u32,
+    params: u29,
+    scopes: u29,
+};
+
+pub const Data = struct {
+    symbols: std.MultiArrayList(Symbol) = .{},
+    packages: std.MultiArrayList(Package) = .{},
+    conses: std.MultiArrayList(Cons) = .{},
+    closures: std.MultiArrayList(Closure) = .{},
+    strings: std.ArrayListUnmanaged([]u8) = .{},
+
+    pub fn deinit(self: *Data, gpa: std.mem.Allocator) void {
+        inline for (std.meta.fields(Data)) |info| {
+            @field(self, info.name).deinit(gpa);
+        }
+    }
+};
+
+test "initialize wisp" {
+    var data = Data{};
+    defer data.deinit(std.testing.allocator);
+}
 
 pub fn encodeFixnum(x: u30) u32 {
     return x << 2;
