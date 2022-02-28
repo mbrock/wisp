cb324698853b8278f43f6db3a85bc65b71fa07d6
Mikael Brockman <mikael@brockman.se>
2022-02-28 10:50:35 +0200

    <mbrock> 2 files changed (+174/-146)

--- src/word.zig
+++ src/word.zig
@@ -17,141 +17,150 @@
 //
 
 const std = @import("std");
+const assert = std.debug.assert;
+const same = std.testing.expectEqual;
+
 const util = @import("./util.zig");
 
-pub const W = struct {
-    pub const Int = packed struct { t: u02 = 0000, v: u30 };
-    pub const Duo = packed struct { t: u03 = 0001, v: u28, e: u1 };
-    pub const Sym = packed struct { t: u03 = 0002, v: u28, e: u1 };
-    pub const Fun = packed struct { t: u03 = 0003, v: u28, e: u1 };
-    pub const Vec = packed struct { t: u03 = 0005, v: u28, e: u1 };
-    pub const Str = packed struct { t: u03 = 0006, v: u28, e: u1 };
-    pub const Chr = packed struct { t: u11 = 0007, v: u21 };
-    pub const Pkg = packed struct { t: u11 = 0015, v: u20, e: u1 };
-    pub const Sys = packed struct { t: u14 = 0023, v: u19 };
-    pub const Nil = packed struct { t: u32 = 2071 };
-    pub const Zap = packed struct { t: u32 = 4119 };
-    pub const Ct0 = packed struct { t: u11 = 0031, v: u20, e: u1 };
+pub const Tag = enum(u5) {
+    int,
+    duo = 0x11,
+    sym = 0x12,
+    fun = 0x13,
+    vec = 0x14,
+    str = 0x15,
+    chr = 0x16,
+    pkg = 0x17,
+    sys = 0x18,
+    ct0 = 0x19,
 };
 
-pub const Dat = struct {
-    pub const Duo = struct { car: u32, cdr: u32 };
-    pub const Fun = struct { env: u32, exp: u32 };
-    pub const Str = struct { idx: u32, len: u32 };
-    pub const Pkg = struct { nam: u32, sym: u32 = nil };
+pub const Era = enum(u1) { e0, e1 };
+pub const Ptr = packed struct {
+    era: Era,
+    idx: u26,
+    tag: Tag,
 
-    pub const Sym = struct {
-        txt: u32,
-        pkg: u32,
-        val: u32 = zap,
-    };
+    pub fn make(tag: Tag, idx: u26, era: Era) Ptr {
+        return .{ .era = era, .idx = idx, .tag = tag };
+    }
 
-    pub const Ct0 = struct {
-        hop: u32,
-        env: u32,
-        fun: u32,
-        arg: u32 = nil,
-        exp: u32 = nil,
-    };
+    pub fn from(x: u32) Ptr {
+        return @bitCast(Ptr, x);
+    }
+
+    pub fn word(ptr: Ptr) u32 {
+        return @bitCast(u32, ptr);
+    }
 };
 
-pub const Tag = util.DeclEnum(W, u16);
+pub const Sys = packed struct {
+    idx: u27,
+    tag: u5 = @enumToInt(Tag.sys),
 
-pub fn raw(w: anytype) u32 {
-    return @bitCast(u32, w);
-}
-
-pub const zap = raw(W.Zap{});
-pub const nil = raw(W.Nil{});
-
-pub fn mkint(x: anytype) W.Int {
-    return W.Int{ .v = @intCast(u30, x) };
-}
-
-pub fn tagword(comptime tag: Tag) type {
-    return @field(W, @tagName(tag));
-}
-
-pub fn wordtag(x: u32) Tag {
-    if (x == 2071) return .Nil;
-    if (x == 4119) return .Zap;
-
-    return switch (@intCast(u3, x & ~@as(u3, 0))) {
-        0, 4 => .Int,
-        1 => .Duo,
-        2 => .Sym,
-        3 => .Fun,
-        5 => .Vec,
-        6 => .Str,
-        7 => switch (@intCast(u11, x & ~@as(u11, 0))) {
-            07 => Tag.Chr,
-            15 => Tag.Pkg,
-            23 => Tag.Sys,
-            31 => Tag.Ct0,
-            else => unreachable,
-        },
-    };
-}
-
-pub fn tagtype(tag: Tag) type {
-    return @field(W, @tagName(tag));
-}
-
-pub fn as(comptime tag: Tag, x: u32) tagtype(tag) {
-    return @bitCast(tagtype(tag), x);
-}
-
-pub fn dat(comptime tag: Tag) type {
-    return @field(Dat, @tagName(tag));
-}
-
-pub fn tabtype(comptime tag: Tag) type {
-    return switch (tag) {
-        .Duo => Tab(Dat.Duo),
-        .Sym => Tab(Dat.Sym),
-        .Str => Tab(Dat.Str),
-        .Pkg => Tab(Dat.Pkg),
-        .Ct0 => Tab(Dat.Ct0),
-        else => void,
-    };
-}
-
-pub const Tabs = struct {
-    Duo: Tab(Dat.Duo) = .{},
-    Sym: Tab(Dat.Sym) = .{},
-    Str: Tab(Dat.Str) = .{},
-    Pkg: Tab(Dat.Pkg) = .{},
-    Ct0: Tab(Dat.Ct0) = .{},
+    pub fn word(sys: Sys) u32 {
+        return @bitCast(u32, sys);
+    }
 };
 
-pub const Era = enum(u1) { old, new };
+pub const nil = (Sys{ .idx = 0 }).word();
+pub const zap = (Sys{ .idx = 1 }).word();
 
-pub fn tagval(comptime tag: Tag) type {
-    const x = as(tag, 0);
-    return std.meta.Int(.unsigned, @bitSizeOf(@TypeOf(x.v)));
+test "nil, zap" {
+    try same(0b11000000000000000000000000000000, nil);
+    try same(0b11000000000000000000000000000001, zap);
 }
 
-pub fn ptr(comptime tag: Tag, val: anytype, era: Era) u32 {
-    return @bitCast(u32, tagword(tag){
-        .v = @intCast(tagval(tag), val),
-        .e = @enumToInt(era),
-    });
+pub fn Dat(comptime t: Tag) type {
+    return switch (t) {
+        .int => void,
+        .duo => struct { car: u32, cdr: u32 },
+        .sym => struct { str: u32, pkg: u32, val: u32 },
+        .fun => struct { env: u32, exp: u32 },
+        .vec => struct { idx: u32, len: u32 },
+        .str => struct { idx: u32, len: u32 },
+        .chr => void,
+        .pkg => struct { nam: u32, sym: u32 },
+        .sys => void,
+        .ct0 => struct { hop: u32, env: u32, fun: u32, arg: u32, exp: u32 },
+    };
+}
+
+pub fn tagOf(x: u32) Tag {
+    return if (x & (1 << 31) == 0)
+        .Int
+    else
+        Ptr.from(x).t;
 }
 
 pub const Orb = std.mem.Allocator;
 pub const Bin = std.ArrayListUnmanaged(u8);
-pub const Tab = std.MultiArrayList;
+
+pub const Err = error{Bad};
+
+pub fn Tab(comptime tag: Tag) type {
+    return struct {
+        const This = @This();
+
+        era: Era,
+        list: std.MultiArrayList(Dat(tag)) = .{},
+
+        const Col = std.meta.FieldEnum(Dat(tag));
+        const prefix: u32 = @enumToInt(tag) << (32 - 5);
+
+        pub fn new(tab: *This, orb: Orb, val: Dat(tag)) !Ptr {
+            try tab.list.append(orb, val);
+            return Ptr.make(tag, @intCast(u26, tab.list.len - 1), tab.era);
+        }
+
+        pub fn get(tab: This, ptr: Ptr) !Dat(tag) {
+            assert(ptr.tag == tag);
+            assert(ptr.era == tab.era);
+            return tab.list.get(ptr.idx);
+        }
+
+        pub fn set(tab: *This, comptime col: Col, ptr: Ptr, val: u32) void {
+            tab.list.items(col)[ptr.idx] = val;
+        }
+    };
+}
+
+pub const Tabs = struct {
+    duo: Tab(.duo),
+    sym: Tab(.sym),
+    fun: Tab(.fun),
+    vec: Tab(.vec),
+    str: Tab(.str),
+    pkg: Tab(.pkg),
+    ct0: Tab(.ct0),
+
+    pub fn init(era: Era) Tabs {
+        return .{
+            .duo = .{ .era = era },
+            .sym = .{ .era = era },
+            .fun = .{ .era = era },
+            .vec = .{ .era = era },
+            .str = .{ .era = era },
+            .pkg = .{ .era = era },
+            .ct0 = .{ .era = era },
+        };
+    }
+};
 
 pub const Vat = struct {
     orb: Orb,
-    era: Era = .new,
+    era: Era = .e0,
     bin: Bin = .{},
-    tabs: Tabs = .{},
+    tabs: Tabs,
 
-    pub fn init(orb: Orb) !Vat {
-        var vat = Vat{ .orb = orb };
-        try vat.tabs.Pkg.append(orb, Dat.Pkg{
-            .nam = try vat.newstr("LISP"),
+    pub fn init(orb: Orb, era: Era) !Vat {
+        var vat = Vat{
+            .orb = orb,
+            .tabs = Tabs.init(era),
+        };
+
+        _ = try vat.new(.pkg, .{
+            .nam = (try vat.newstr("WISP")).word(),
             .sym = nil,
         });
 
@@ -161,63 +170,67 @@ pub const Vat = struct {
     pub fn deinit(vat: *Vat) void {
         vat.bin.deinit(vat.orb);
         inline for (std.meta.fields(Tabs)) |field| {
-            @field(vat.tabs, field.name).deinit(vat.orb);
+            @field(vat.tabs, field.name).list.deinit(vat.orb);
         }
     }
 
-    pub fn new(vat: *Vat, comptime tag: Tag, x: dat(tag)) !u32 {
-        var tab = &@field(vat.tabs, @tagName(tag));
-        try tab.append(vat.orb, x);
-        return ptr(tag, 0, vat.era);
+    pub fn tab(vat: *Vat, comptime tag: Tag) *Tab(tag) {
+        return &@field(vat.tabs, @tagName(tag));
     }
 
-    pub fn newstr(vat: *Vat, txt: []const u8) !u32 {
-        const str = Dat.Str{
-            .idx = raw(mkint(vat.tabs.Str.len)),
-            .len = raw(mkint(txt.len)),
-        };
+    pub fn new(vat: *Vat, comptime tag: Tag, val: Dat(tag)) !Ptr {
+        return vat.tab(tag).new(vat.orb, val);
+    }
+
+    pub fn get(vat: *Vat, comptime tag: Tag, ptr: Ptr) Dat(tag) {
+        return vat.tab(tag).get(ptr);
+    }
+
+    pub fn newstr(vat: *Vat, txt: []const u8) !Ptr {
         try vat.bin.appendSlice(vat.orb, txt);
-        return vat.new(.Str, str);
+        return vat.new(.str, .{
+            .idx = @intCast(u32, vat.bin.items.len - txt.len),
+            .len = @intCast(u32, txt.len),
+        });
     }
 
-    pub fn get(vat: *Vat, comptime tag: Tag, ptr: u32) dat(tag) {
-        const w = as(tag, ptr);
-        assert(w.e == vat.era);
-        return @field(vat.tabs, @tagName(tag)).get(w.v);
+    pub fn strslice(vat: Vat, ptr: Ptr) ![]const u8 {
+        const str = vat.get(.str, ptr);
+        return vat.bin.items[str.idx .. str.len - 1];
     }
 
-    pub fn intern(vat: *Vat, txt: []const u8, ppkg: u32) !u32 {
-        const pkg = vat.get(.Pkg, ppkg);
+    pub fn intern(vat: *Vat, txt: []const u8, pkgptr: Ptr) !Ptr {
+        const symstrs = vat.tabs.sym.list.items(.txt);
+        const pkg = vat.get(.pkg, pkgptr);
+        var duoptr = vat.get(.duo, pkg.sym);
 
-        var pduo = vat.tabs.Pkg.items(.sym)[pkg.v];
-        const txts = vat.tabs.Sym.items(.txt);
-
-        while (pduo != nil) {
-            const duo = vat.get(.Duo, pduo);
-            const txt = txts[as(.Sym, duo.car)];
+        while (duoptr != nil) {
+            const duo = vat.get(.duo, duoptr);
+            const symptr = Ptr.from(duo.car);
+            const strptr = symstrs[symptr.idx];
+            if (std.mem.eql(u8, txt, vat.strslice(strptr))) {
+                return symptr;
+            } else {
+                duoptr = duo.cdr;
+            }
         }
+
+        const symptr = try vat.new(.sym, .{
+            .str = try vat.newstr(txt),
+            .pkg = pkgptr.word(),
+            .val = zap,
+        });
+
+        vat.tabs.pkg.set(.sym, try vat.new(.duo, .{
+            .car = symptr,
+            .cdr = pkg.sym,
+        }));
+
+        return symptr;
     }
 };
 
 test "vat" {
-    var vat = try Vat.init(std.testing.allocator);
+    var vat = try Vat.init(std.testing.allocator, .e0);
     defer vat.deinit();
-}
-
-test "foo" {
-    const x: u32 = 0o7 + (123 << 11);
-    const chr = @bitCast(W.Chr, x);
-    try std.testing.expectEqual(Tag.Chr, wordtag(x));
-    try std.testing.expectEqual(0o7, chr.t);
-    try std.testing.expectEqual(123, chr.v);
-    try std.testing.expectEqual(0o4027, @bitCast(u32, W.Nil{}));
-
-    const fun = W.Fun{ .v = 1, .e = 1 };
-    try std.testing.expectEqual(
-        0b10000000000000000000000000001011,
-        @bitCast(u32, fun),
-    );
-
-    const fun2 = as(.Fun, 0b10000000000000000000000000001011);
-    try std.testing.expectEqual(fun, fun2);
 }
