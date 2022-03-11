80a7002c3ae14486b647a8bf88237ce6b782a0f6
Mikael Brockman <mikael@brockman.se>
2022-03-11 13:23:27 +0200

    Work on folding and indentation in CodeMirror

--- ts/language.ts
+++ ts/language.ts
@@ -1,18 +1,58 @@
-import { parser } from "./wisp.grammar";
-import { continuedIndent, delimitedIndent, foldInside, foldNodeProp, indentNodeProp, LanguageSupport, LRLanguage } from "@codemirror/language";
-import { styleTags, tags } from "@codemirror/highlight";
+// -*- fill-column: 64; -*-
+//
+// This file is part of Wisp.
+//
+// Wisp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License
+// as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+//
+// Wisp is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General
+// Public License along with Wisp. If not, see
+// <https://www.gnu.org/licenses/>.
+//
+
+import { parser } from "./wisp.grammar"
+import { styleTags, tags } from "@codemirror/highlight"
 import { completeFromList } from "@codemirror/autocomplete"
+import {
+  continuedIndent, foldNodeProp, indentNodeProp,
+  LanguageSupport, LRLanguage
+} from "@codemirror/language"
+import { SyntaxNode } from "@lezer/common"
+
+const hang = (n: number) => (node: SyntaxNode) => {
+  let kids = node.getChildren("Form")
+  let first = kids[n - 1], last = node.lastChild;
+  if (first && first.to < last.from) {
+    return {
+      from: first.to,
+      to: last.type.isError ? node.to : last.from
+    }
+  }
+  return null
+}
 
 export const wispLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Form: continuedIndent({ units: 2 }),
+        Defun: continuedIndent(),
+        Let: continuedIndent(),
+        Lambda: continuedIndent(),
       }),
       foldNodeProp.add({
-        Form: foldInside,
+        Defun: hang(2),
+        Let: hang(1),
+        Lambda: hang(1),
       }),
       styleTags({
+        "defun let lambda": tags.keyword,
         Symbol: tags.variableName,
         String: tags.string,
         Integer: tags.number,
