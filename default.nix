c53d98bfdd6c14c0b47962410b32d8c517c6f2be
Mikael Brockman <mikael@brockman.se>
2022-03-15 09:28:47 +0200

    <mbrock> 6 files changed (+179/-2)

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ zig, stdenv }:
+
+stdenv.mkDerivation {
+  name = "wisp";
+  version = "0.7";
+  src = ./.;
+  buildInputs = [zig];
+  XDG_CACHE_HOME = ".cache";
+  testPhase = "zig build test";
+  buildPhase = "zig build";
+  installPhase = ''
+    mkdir -p $out/bin
+    cp zig-out/bin/wisp/* $out/bin
+  '';
+}
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url =
+      github:nixos/nixpkgs/nixpkgs-unstable;
+
+    flake-utils.url =
+      github:numtide/flake-utils;
+
+    zig.url = github:roarkanize/zig-overlay;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, zig }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              zigpkgs = inputs.zig.packages.${system};
+            })
+          ];
+        };
+      in rec {
+        devShell = packages.wisp;
+        defaultPackage = packages.wisp;
+        packages = rec {
+          zig = inputs.zig.packages.${system}.master.latest;
+          wisp = pkgs.callPackage ./default.nix {
+            inherit zig;
+          };
+        };
+      }
+    );
+}
