4061d33412f7d0782234288b5f5f7ebdcfa2d7a4
Mikael Brockman <mikael@brockman.se>
2022-03-22 09:12:02 +0200

    wisp-mode: some indentation stuff

--- etc/wisp.el
+++ etc/wisp.el
@@ -1,6 +1,21 @@
 (define-derived-mode wisp-mode
   lisp-mode "Wisp"
   "Major mode for Wisp code."
-  (setq-local inferior-lisp-program "zig build run -- repl"))
+  (setq-local inferior-lisp-program "zig build run -- repl")
+
+  (define-common-lisp-style "wisp"
+    "This style is for Wisp."
+    (:inherit "basic")
+    (:eval
+     (whitespace-mode 1))
+    (:variables
+     (fill-column 64))
+    (:indentation
+     (fn (as lambda))
+     (handle (4 &rest (&whole 2 &rest 1)))
+     (call-with-prompt 1)
+     (returning 1)))
+
+  (common-lisp-set-style "wisp"))
 
 (provide 'wisp)
