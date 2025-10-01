b8e29dba0ea61ebaf2db5e3e7f82e238dfc0a15d
Mikael Brockman <mikael@brockman.se>
2025-10-01 21:30:07 +0300

    wasi stubs to make the web frontend semi-functional
    I forgot how to use the web frontend, though.
    It looks cool, but it doesn't do anything.
    Will investigate further; stay tuned.

--- web/wasi.js
+++ web/wasi.js
@@ -17,32 +17,33 @@
 // <https://www.gnu.org/licenses/>.
 //
 
-const WASI_ESUCCESS = 0
-const WASI_STDOUT_FILENO = 1
-const WASI_STDERR_FILENO = 2
+const WASI_ESUCCESS = 0;
+const WASI_STDOUT_FILENO = 1;
+const WASI_STDERR_FILENO = 2;
+const WASI_EBADF = 8;
 
 const CLOCK = {
   REALTIME: 0,
   MONOTONIC: 1,
-}
+};
 
 export default class WASI {
-  memory
-  buffers
+  memory;
+  buffers;
 
   constructor() {
     this.buffers = {
       [WASI_STDOUT_FILENO]: [],
       [WASI_STDERR_FILENO]: [],
-    }
+    };
   }
 
   setMemory(memory) {
-    this.memory = memory
+    this.memory = memory;
   }
 
   getDataView() {
-    return new DataView(this.memory.buffer)
+    return new DataView(this.memory.buffer);
   }
 
   exports() {
@@ -53,53 +54,78 @@ export default class WASI {
       fd_prestat_dir_name() {},
 
       fd_write: (fd, iovs, iovsLen, nwritten) => {
-        const view = this.getDataView()
-        let written = 0
+        const view = this.getDataView();
+        let written = 0;
 
         const buffers = Array.from({ length: iovsLen }, (_, i) => {
-          const ptr = iovs + i * 8
-          const buf = view.getUint32(ptr, !0)
-          const bufLen = view.getUint32(ptr + 4, !0)
+          const ptr = iovs + i * 8;
+          const buf = view.getUint32(ptr, !0);
+          const bufLen = view.getUint32(ptr + 4, !0);
 
-          return new Uint8Array(
-            this.memory.buffer, buf, bufLen
-          )
-        })
+          return new Uint8Array(this.memory.buffer, buf, bufLen);
+        });
 
         // XXX: verify that this handles multiple lines correctly
         for (let iov of buffers) {
-          const newline = 10
-          let i = 0
-          let newlineIndex = iov.lastIndexOf(newline, i)
+          const newline = 10;
+          let i = 0;
+          let newlineIndex = iov.lastIndexOf(newline, i);
           if (newlineIndex > -1) {
-            let line = "", decoder = new TextDecoder
+            let line = "",
+              decoder = new TextDecoder();
 
             for (let buffer of this.buffers[fd]) {
-              line += decoder.decode(buffer, { stream: true })
+              line += decoder.decode(buffer, { stream: true });
             }
 
-            line += decoder.decode(iov.slice(0, newlineIndex))
+            line += decoder.decode(iov.slice(0, newlineIndex));
 
-            if (fd === WASI_STDOUT_FILENO) console.log(line)
-            else if (fd === WASI_STDERR_FILENO) console.warn(line)
+            if (fd === WASI_STDOUT_FILENO) console.log(line);
+            else if (fd === WASI_STDERR_FILENO) console.warn(line);
 
-            this.buffers[fd] = [iov.slice(newlineIndex + 1)]
-            i = newlineIndex + 1
+            this.buffers[fd] = [iov.slice(newlineIndex + 1)];
+            i = newlineIndex + 1;
           }
 
-          this.buffers[fd].push(new Uint8Array(iov.slice(i)))
+          this.buffers[fd].push(new Uint8Array(iov.slice(i)));
 
-          written += iov.byteLength
+          written += iov.byteLength;
         }
 
+        view.setUint32(nwritten, written, !0);
 
-        view.setUint32(nwritten, written, !0)
+        return WASI_ESUCCESS;
+      },
 
-        return WASI_ESUCCESS
+      fd_pwrite: (fd, iovs, iovsLen, offset, nwritten) => {
+        // Let pwrite append for stdio streams.
+        // "You Only Live Once"
+        if (fd === WASI_STDOUT_FILENO || fd === WASI_STDERR_FILENO) {
+          return this.exports().fd_write(fd, iovs, iovsLen, nwritten);
+        }
+
+        return WASI_EBADF;
       },
 
       fd_close() {},
       fd_read() {},
+      fd_pread() {
+        // "I was in Nashville, Tennessee last year.
+        // After the show I went to a Waffle House.
+        // I'm not proud of it, I was hungry.
+        // And I'm alone, I'm eating and I'm reading a book, right?
+        // Waitress walks over to me: 'Hey, whatcha readin' for?'
+        // Isn't that the weirdest fuckin' question you've ever heard?
+        // Not what am I reading, but what am I reading FOR?
+        // Well, goddamnit, ya stumped me!"
+
+        return WASI_EBADF;
+      },
+
+      fd_seek() {},
+
+      fd_fdstat_get() {},
+      fd_fdstat_set_flags() {},
 
       path_open() {},
       path_rename() {},
@@ -110,33 +136,29 @@ export default class WASI {
       fd_filestat_get() {},
 
       random_get: (buf_ptr, buf_len) => {
-        const buffer = new Uint8Array(this.memory.buffer, buf_ptr, buf_len)
-        crypto.getRandomValues(buffer)
+        const buffer = new Uint8Array(this.memory.buffer, buf_ptr, buf_len);
+        crypto.getRandomValues(buffer);
 
-        return 0
+        return 0;
       },
 
-      clock_time_get: (
-        clock_id,
-        _precision,
-        timestamp_out,
-      ) => {
-        const view = this.getDataView()
+      clock_time_get: (clock_id, _precision, timestamp_out) => {
+        const view = this.getDataView();
 
         switch (clock_id) {
           case CLOCK.REALTIME:
           case CLOCK.MONOTONIC: {
-            const t = BigInt(Date.now()) * BigInt(1e6)
-            view.setBigUint64(timestamp_out, t, true)
-            break
+            const t = BigInt(Date.now()) * BigInt(1e6);
+            view.setBigUint64(timestamp_out, t, true);
+            break;
           }
 
           default:
-            throw new Error("unhandled clock type")
+            throw new Error("unhandled clock type");
         }
 
-        return 0
+        return 0;
       },
-    }
+    };
   }
 }
